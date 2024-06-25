e6335c83e451250014df4ad3b5d6f3d30e035791
Jamkris <dltmdgus1412@gmail.com>
2024-06-25 23:18:27 +0900

    add(#4) :: 데이터 받기 성공
    + 높이 데이터 받기
    + props로 style 줬음

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,13 +1,32 @@
-// 라이브러리
-import React from "react";
-
-//파일
-import * as _ from "./style";
+import React, { useEffect, useState } from 'react';
+import * as _ from './style';
 
 const Auth = () => {
-  return <_.Auth_Container>
-    
-  </_.Auth_Container>;
+	const [statusBarHeight, setStatusBarHeight] = useState(0);
+
+	const onMessageEvent = (e: MessageEvent) => {
+		e.stopPropagation();
+		console.log(e.data);
+		if (typeof e.data === 'string') {
+			const height = parseInt(e.data, 10);
+			if (!isNaN(height)) {
+				setStatusBarHeight(height);
+			}
+		}
+	};
+
+	useEffect(() => {
+		window.addEventListener('message', onMessageEvent, { capture: true });
+		return () => window.removeEventListener('message', onMessageEvent);
+	}, []);
+
+	return (
+		<_.Auth_Container>
+			<_.Auth_Layout StatusBarSize={`${statusBarHeight}px`}>
+				Data {statusBarHeight}
+			</_.Auth_Layout>
+		</_.Auth_Container>
+	);
 };
 
 export default Auth;
--- src/pages/auth/style.ts
+++ src/pages/auth/style.ts
@@ -1,7 +1,11 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Auth_Container = styled.div`
   width: 100vw;
   height: 100vh;
   background: linear-gradient(180deg, #fbc7d4 0%, #9796f0 100%);
+`;
+
+export const Auth_Layout = styled.div<{ StatusBarSize?: string }>`
+  padding-top: ${(props) => props.StatusBarSize || '54px'};
 `;
