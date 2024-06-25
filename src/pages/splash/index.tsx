5d912dcfd68dfe9436e21845243a5bd8d2caac2d
Jamkris <dltmdgus1412@gmail.com>
2024-06-25 13:52:43 +0900

    add :: 로고 가져오기

--- src/pages/splash/index.tsx
+++ src/pages/splash/index.tsx
@@ -1,11 +1,18 @@
+//라이브러리
 import React from 'react';
 
+//파일
+import * as _ from './style';
+import { Harp_Logo } from 'assets/image/HARP_LOGO.tsx';
+
 const Splash = () => {
-  return (
-    <div>
-      Test
-    </div>
-  );
+	return (
+		<_.Splash_Container>
+			<_.Splash_Logo>
+				<Harp_Logo />
+			</_.Splash_Logo>
+		</_.Splash_Container>
+	);
 };
 
 export default Splash;
--- src/pages/splash/style.ts
+++ src/pages/splash/style.ts
@@ -1,1 +1,10 @@
 import styled from 'styled-components';
+
+export const Splash_Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
+
+export const Splash_Logo = styled.div`
+
+`;
