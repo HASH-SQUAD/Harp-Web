2bec56c52408db5e98af363590f9639133e46943
추성우 <cntjddn715@gmail.com>
2024-06-26 14:05:57 +0900

    chore(#4) :: 말풍선

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -4,14 +4,29 @@ import React from "react";
 // 파일
 import * as _ from "./style";
 import useStatusBarHeight from "../../hooks/useStatusBarHeight";
+import RoundedTriangle from "assets/image/RoundedTriangle";
 
 const Auth = () => {
   const statusBarHeight = useStatusBarHeight();
+  const title = "How are\nyou\nPlanning?";
 
   return (
     <_.Auth_Container>
       <_.Auth_Layout StatusBarSize={`${statusBarHeight}px`}>
-        Data {statusBarHeight}
+        <_.Auth_Title_Layout>
+          <_.Auth_Title_Big>{title}</_.Auth_Title_Big>
+          <_.Auth_Title_Small>하프와 함께하는 여행 계획</_.Auth_Title_Small>
+        </_.Auth_Title_Layout>
+        <_.Auth_Button_Layout>
+          <_.Auth_Button_Ballon>
+            <_.Auth_Button_Ballon_Title>
+              3초만에 하는 빠른 회원가입🚀
+            </_.Auth_Button_Ballon_Title>
+          </_.Auth_Button_Ballon>
+          <_.Auth_Button_Ballon_SVG>
+            <RoundedTriangle />
+          </_.Auth_Button_Ballon_SVG>
+        </_.Auth_Button_Layout>
       </_.Auth_Layout>
     </_.Auth_Container>
   );
--- src/pages/auth/style.ts
+++ src/pages/auth/style.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Auth_Container = styled.div`
   width: 100vw;
@@ -7,5 +7,46 @@ export const Auth_Container = styled.div`
 `;
 
 export const Auth_Layout = styled.div<{ StatusBarSize?: string }>`
-  padding-top: ${(props) => props.StatusBarSize || '54px'};
+  padding: ${(props) => props.StatusBarSize || "54px"} 40px 52px 40px;
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const Auth_Title_Layout = styled.div`
+  padding-top: 48px;
+`;
+
+export const Auth_Title_Big = styled.p`
+  white-space: pre-line;
+  font-size: 48px;
+  font-weight: 700;
+  color: white;
+`;
+
+export const Auth_Title_Small = styled.p`
+  color: white;
+  padding-top: 10px;
+  font-weight: 400;
+`;
+
+export const Auth_Button_Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+`;
+
+export const Auth_Button_Ballon = styled.div`
+  background-color: white;
+  padding: 10px 16px;
+  border-radius: 30px;
+`;
+
+export const Auth_Button_Ballon_Title = styled.p``;
+
+export const Auth_Button_Ballon_SVG = styled.div`
+  position: absolute;
+  top: 40px;
 `;
