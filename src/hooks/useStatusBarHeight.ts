a448cc82d2b38bf506ec4b4c984f353d0425611b
추성우 <cntjddn715@gmail.com>
2024-06-26 11:41:12 +0900

    refactor(#4) :: statusBarHeight hook 분리
    statusBarHeight를 구하는 함수를 hook으로 분리해서 사용하도록 만들었습니다.

---
+++ src/hooks/useStatusBarHeight.ts
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+const useStatusBarHeight = () => {
+  const [statusBarHeight, setStatusBarHeight] = useState(0);
+
+  const onMessageEvent = (e: MessageEvent) => {
+    e.stopPropagation();
+    console.log(e.data);
+    if (typeof e.data === "string") {
+      const height = parseInt(e.data, 10);
+      if (!isNaN(height)) {
+        setStatusBarHeight(height);
+      }
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("message", onMessageEvent, { capture: true });
+    return () => window.removeEventListener("message", onMessageEvent);
+  }, []);
+
+  return statusBarHeight;
+};
+
+export default useStatusBarHeight;
--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,32 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import * as _ from './style';
+// 라이브러리
+import React from "react";
+
+// 파일
+import * as _ from "./style";
+import useStatusBarHeight from "../../hooks/useStatusBarHeight";
 
 const Auth = () => {
-	const [statusBarHeight, setStatusBarHeight] = useState(0);
+  const statusBarHeight = useStatusBarHeight();
 
-	const onMessageEvent = (e: MessageEvent) => {
-		e.stopPropagation();
-		console.log(e.data);
-		if (typeof e.data === 'string') {
-			const height = parseInt(e.data, 10);
-			if (!isNaN(height)) {
-				setStatusBarHeight(height);
-			}
-		}
-	};
-
-	useEffect(() => {
-		window.addEventListener('message', onMessageEvent, { capture: true });
-		return () => window.removeEventListener('message', onMessageEvent);
-	}, []);
-
-	return (
-		<_.Auth_Container>
-			<_.Auth_Layout StatusBarSize={`${statusBarHeight}px`}>
-				Data {statusBarHeight}
-			</_.Auth_Layout>
-		</_.Auth_Container>
-	);
+  return (
+    <_.Auth_Container>
+      <_.Auth_Layout StatusBarSize={`${statusBarHeight}px`}>
+        Data {statusBarHeight}
+      </_.Auth_Layout>
+    </_.Auth_Container>
+  );
 };
 
 export default Auth;
