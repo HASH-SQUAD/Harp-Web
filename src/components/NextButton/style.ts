a847da5865f77a9138d615357760babdcb4e2249
Jamkris <dltmdgus1412@gmail.com>
2024-06-28 10:14:43 +0900

    refactor(#14) :: NextButton 포지션 수정

--- src/components/NextButton/style.ts
+++ src/components/NextButton/style.ts
@@ -7,7 +7,7 @@ export const NextButton_Container = styled.div<{ State?: boolean }>`
   display: flex;
   flex-direction: row;
   justify-content: center;
-  position: absolute;
+  position: fixed;
   z-index: 9999;
   bottom: 0;
   background-color: ${(props) =>
