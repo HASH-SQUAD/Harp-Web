58df15e7e53f491e9ea4f277946691792b678791
Jamkris <dltmdgus1412@gmail.com>
2024-07-03 14:37:14 +0900

    add(#16) :: 타이틀 작성

--- src/pages/surveyMBTI/index.tsx
+++ src/pages/surveyMBTI/index.tsx
@@ -4,14 +4,32 @@ import React from 'react';
 //파일
 import * as _ from './style';
 import useStatusBarHeight from 'hooks/useStatusBarHeight';
+import Header from 'components/Header';
 
 const SurveyMBTI = () => {
   const statusBarHeight = useStatusBarHeight();
   return (
     <_.SurveyMBTI_Container>
-      <_.SurveyMBTI_Layout
-        StatusBarSize={`${statusBarHeight}px`}
-      ></_.SurveyMBTI_Layout>
+      <_.SurveyMBTI_Layout StatusBarSize={`${statusBarHeight}px`}>
+        <Header
+          title=""
+          StatusBar={3}
+          buttonState=""
+          onClickMethod={() => {
+            return;
+          }}
+        />
+
+        <_.SurveyMBTI_ProgressText>3/3</_.SurveyMBTI_ProgressText>
+
+        <_.SurveyMBTI_MainText>MBTI가 무엇인가요?</_.SurveyMBTI_MainText>
+
+        <_.SurveyMBTI_SubText>
+          <div>여행 계획을 짤 때 참고할게요 :-)</div>
+
+          <div>건너뛰기</div>
+        </_.SurveyMBTI_SubText>
+      </_.SurveyMBTI_Layout>
     </_.SurveyMBTI_Container>
   );
 };
--- src/pages/surveyMBTI/style.ts
+++ src/pages/surveyMBTI/style.ts
@@ -1,3 +1,4 @@
+import { theme } from 'lib/utils/style/theme';
 import styled from 'styled-components';
 
 export const SurveyMBTI_Container = styled.div``;
@@ -7,4 +8,28 @@ export const SurveyMBTI_Layout = styled.div<{ StatusBarSize?: string }>`
   display: flex;
   height: 95%;
   flex-direction: column;
+`;
+
+export const SurveyMBTI_ProgressText = styled.div`
+  color: var(--gray-300, #a5a5a5);
+  font-weight: 400;
+  font-size: 14px;
+  text-align: right;
+`;
+
+export const SurveyMBTI_MainText = styled.div`
+  color: ${theme.gray.black};
+  font-weight: 700;
+  font-size: 24px;
+`;
+
+export const SurveyMBTI_SubText = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  font-weight: 400;
+  font-size: 16px;
+  color: var(--gray-400, #424242);
 `;
