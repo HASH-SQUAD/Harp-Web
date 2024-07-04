74011fe0d56010a782ac4ffe7e74a1500fa20d66
Jamkris <dltmdgus1412@gmail.com>
2024-07-04 17:38:50 +0900

    refactor(#16) :: 색상 수정

--- src/pages/surveyStyle/style.ts
+++ src/pages/surveyStyle/style.ts
@@ -11,7 +11,7 @@ export const SurveyStyle_Layout = styled.div<{ StatusBarSize?: string }>`
 `;
 
 export const SurveyStyle_ProgressText = styled.div`
-  color: var(--gray-300, #a5a5a5);
+  color: ${theme.gray[3]};
   font-weight: 400;
   font-size: 14px;
   text-align: right;
