33036a69dfb57d8fde3e41edba7648446baeb452
Jamkris <dltmdgus1412@gmail.com>
2024-07-04 19:33:05 +0900

    add(#18) :: 파일 & 레이아웃 세팅

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,15 @@
+//라이브러리
+import React from 'react';
+
+//파일
+import * as _ from './style';
+
+const Home = () => {
+  return (
+    <_.Home_Container>
+      <_.Home_Layout></_.Home_Layout>
+    </_.Home_Container>
+  );
+};
+
+export default Home;
---
+++ src/pages/home/style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Home_Container = styled.div`
+
+`
+
+export const Home_Layout = styled.div`
+  
+`
