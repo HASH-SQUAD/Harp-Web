0c2251cbfb93078e370c6602039a3c31bfd0492d
추성우 <cntjddn715@gmail.com>
2024-07-08 00:32:42 +0900

    chore :: surveyfood layout 수정

--- src/pages/register/surveyFood/style.ts
+++ src/pages/register/surveyFood/style.ts
@@ -1,9 +1,7 @@
 import { theme } from 'lib/utils/style/theme';
 import styled from 'styled-components';
 
-export const SurveyFood_Container = styled.div``;
-
-export const SurveyFoode_Layout = styled.div<{ StatusBarSize?: string }>`
+export const SurveyFood_Container = styled.div<{ StatusBarSize?: string }>`
   padding: ${(props) => props.StatusBarSize || '54px'} 30px 52px 30px;
   display: flex;
   height: 95%;
