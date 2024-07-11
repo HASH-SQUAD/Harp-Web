cb589bbf912220839293340e0dc7de75826133ca
추성우 <cntjddn715@gmail.com>
2024-07-11 14:45:08 +0900

    chore(#54) :: 해싱이 svg 추가

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,16 @@
+// 라이브러리
+import React from 'react';
+
+// 파일
+import * as _ from './style';
+import Hashingee from 'assets/image/Hashingee';
+
+const NotFound = () => {
+  return (
+    <_.NotFound_Container>
+      <Hashingee />
+    </_.NotFound_Container>
+  );
+};
+
+export default NotFound;
---
+++ src/pages/NotFound/style.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const NotFound_Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const NotFound_Content = styled.div`
+    
+`;
