593ad94338ea4c401b0a1688227f316a08889c4d
추성우 <cntjddn715@gmail.com>
2024-07-27 14:11:10 +0900

    add(#20) :: Detail Image 변경 기능

--- src/pages/plan/detail/index.tsx
+++ src/pages/plan/detail/index.tsx
@@ -1,5 +1,5 @@
 // 라이브러리
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 // 파일
 import * as _ from './style';
@@ -8,18 +8,28 @@ import Header from 'components/Header';
 import PlanDetail from 'assets/image/PlanDetail.png';
 import Camera from 'assets/Icon/Camera';
 import calculateDDay from 'lib/utils/D-Day';
+import { handleImageEdit } from 'lib/utils/handleImageEdit';
 
 const Detail = () => {
   const statusBarHeight = useStatusBarHeight();
-  const planDate = { title: '가나다라마바사', date: '2024-08-09' };
+  const planInfos = {
+    image: PlanDetail,
+    title: '가나다라마바사',
+    date: '2024-08-08'
+  };
+  const [planDetailImage, setPlanDetailImage] = useState(planInfos.image);
 
   return (
     <_.PlanDetail_Layout StatusBarSize={`${statusBarHeight}px`}>
       <Header title="일정" buttonState="닫기" />
-      <_.PlanDetail_Header BackgroundImage={PlanDetail}>
-        <_.PlanDetail_Title>{planDate.title}</_.PlanDetail_Title>
-        <_.PlanDetail_DDay>{calculateDDay(planDate.date)}</_.PlanDetail_DDay>
-        <_.PlanDetail_Camera>
+      <_.PlanDetail_Header BackgroundImage={planDetailImage}>
+        <_.PlanDetail_Title>{planInfos.title}</_.PlanDetail_Title>
+        <_.PlanDetail_DDay>{calculateDDay(planInfos.date)}</_.PlanDetail_DDay>
+        <_.PlanDetail_Camera
+          onClick={() => {
+            handleImageEdit(setPlanDetailImage);
+          }}
+        >
           <Camera />
         </_.PlanDetail_Camera>
       </_.PlanDetail_Header>
