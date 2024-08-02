9c73eba1cf1af37aeabb881a8d4db46bc75e9b9f
추성우 <cntjddn715@gmail.com>
2024-08-03 01:28:15 +0900

    chore(#20) :: TripSchedule 데이터 추가

---
+++ src/data/TripSchedule.ts
@@ -0,0 +1,109 @@
+export const TripSchedule = [
+  [
+    {
+      time: '10:00',
+      activity: '서면 문화를 즐기기',
+      location: '부산 부산진구 서면로68번길 9',
+      recommendation:
+        '현대적인 건축과 어우러진 서면의 다양한 상점과 카페를 즐기실 수 있어요.',
+      storeName: '서면 거리',
+      place_url: 'http://place.map.kakao.com/775828932'
+    },
+    {
+      time: '12:00',
+      activity: '점심 식사',
+      location: '부산 부산진구 서면첨단길 39-11',
+      recommendation:
+        '서면에서 유명한 서면개금밀면숯불 수제불고기에서 맛있는 식사를 즐기실 수 있어요.',
+      storeName: '서면개금밀면숯불 수제불고기',
+      place_url: 'http://place.map.kakao.com/12748921'
+    },
+    {
+      time: '14:00',
+      activity: '해변 산책하기',
+      location: '부산 부산진구 서면로69번가길 4',
+      recommendation:
+        '비앤와이 비치에서 해변을 산책하며 바다의 공기를 즐기세요.',
+      storeName: '비앤와이 비치',
+      place_url: 'http://place.map.kakao.com/422161677'
+    },
+    {
+      time: '17:00',
+      activity: '저녁 식사',
+      location: '부산 부산진구 서면로67번가길 9-1',
+      recommendation:
+        '맛있는 서면 소고기 꼬치 요리를 맛보실 수 있는 기모찌내모찡에서 식사를 즐기세요.',
+      storeName: '기모찌내모찡',
+      place_url: 'http://place.map.kakao.com/1098508674'
+    }
+  ],
+  [
+    {
+      time: '10:00',
+      activity: '문화 체험하기',
+      location: '부산 부산진구 서문로164번길 3',
+      recommendation:
+        '부산 현대미술관에서 다양한 전시물을 감상하며 문화 체험을 하세요.',
+      storeName: '부산 현대미술관',
+      place_url: 'http://place.map.kakao.com/1003500764'
+    },
+    {
+      time: '12:00',
+      activity: '점심 식사',
+      location: '부산 부산진구 부전동 261-5',
+      recommendation:
+        '가족과 함께 즐길 수 있는 미스터덤행복 부침개를 맛보실 수 있는 백반 도마비에서 맛있는 식사를 즐기세요.',
+      storeName: '백반 도마비',
+      place_url: 'http://place.map.kakao.com/317753074'
+    },
+    {
+      time: '14:00',
+      activity: '등산하기',
+      location: '부산 부산진구 금련산로85번길 78',
+      recommendation:
+        '가족과 함께 명상하며 새로운 에너지를 얻을 수 있는 현대골프클럽 카르마 스파 서면 피그먹(SEMAAN Pghmeog)에서 등산을 즐겨보세요.',
+      storeName: '현대골프클럽 카르마 스파 서면 피그먹(SEMAAN Pghmeog)',
+      place_url: 'http://place.map.kakao.com/1899783035'
+    }
+  ],
+  [
+    {
+      time: '09:00',
+      activity: '부산 도넛 맛보기',
+      location: '부산 부산진구 중앙대로646번길 67-8',
+      recommendation:
+        '맛있는 도넛을 맛볼 수 있는 도넛 TREE에서 양주와 함께 시작하세요.',
+      storeName: '도넛 TREE',
+      place_url: 'http://place.map.kakao.com/1160617485'
+    },
+    {
+      time: '12:00',
+      activity: '점심 식사',
+      location: '부산 부산진구 서면로68번길 33',
+      recommendation:
+        '할리포아 삼겹정육식당에서 신선한 고기와 멋진 분위기를 즐겨보세요.',
+      storeName: '할리포아 삼겹정육식당',
+      place_url: 'http://place.map.kakao.com/314236660'
+    }
+  ],
+  [
+    {
+      time: '10:00',
+      activity: '부산 야구 관람',
+      location: '부산 부산진구 시민공원선수촌로 72',
+      recommendation:
+        '부산 롯데 자이언츠 야구 경기를 관람하여 야구의 매력을 느껴보세요.',
+      storeName: '사직야구장',
+      place_url: 'http://place.map.kakao.com/48496500'
+    },
+    {
+      time: '14:00',
+      activity: '감성 카페에서 쉬기',
+      location: '부산 부산진구 서면문화로4번길 37',
+      recommendation:
+        '서면에 위치한 아기곰 대표와 커피 사진을 찍을 수 있는 스페이스 텅에서 감성을 느껴보세요.',
+      storeName: '스페이스 텅',
+      place_url: 'http://place.map.kakao.com/913779573'
+    }
+  ]
+];
