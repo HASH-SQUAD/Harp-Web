f69e1456c0127ab9a214314009f28dacbdd8e0c8
추성우 <cntjddn715@gmail.com>
2024-08-05 00:04:50 +0900

    add(#78) :: 카카오 지도 불러오기

--- src/components/Maps/MiniMap/index.tsx
+++ src/components/Maps/MiniMap/index.tsx
@@ -1,11 +1,37 @@
-// 라이브러리
 import React, { useEffect } from 'react';
-
-// 파일
 import * as _ from './style';
 
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+
 const MiniMap = () => {
-  return <_.MiniMap_Layout />;
+  useEffect(() => {
+    const loadKakaoMap = () => {
+      const script = document.createElement('script');
+      script.src = `https://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=${process.env.REACT_APP_KAKAO_KEY}`;
+      script.onload = () => {
+        window.kakao.maps.load(() => {
+          const container = document.getElementById('map');
+          const options = {
+            center: new window.kakao.maps.LatLng(
+              37.29432708770308,
+              126.99658273529194
+            ),
+            level: 3
+          };
+          const map = new window.kakao.maps.Map(container, options);
+        });
+      };
+      document.head.appendChild(script);
+    };
+
+    loadKakaoMap();
+  }, []);
+
+  return <_.MiniMap_Layout id="map" />;
 };
 
 export default MiniMap;
