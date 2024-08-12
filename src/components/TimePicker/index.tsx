71ba55f870cf8a443cbc184e90b2503f34ff4793
추성우 <cntjddn715@gmail.com>
2024-08-13 01:52:06 +0900

    refactor(#78) :: 함수 재수정 및 timerRef 타입 변경
    timerRef의 타입을 NodeJS.Timeout | null에서 number | null로 변경하라고 했는데 Node.js 환경에서는 전자를, 브라우저에서는 후자를 반환하기에, 두 환경에서 다 적용하기 위해 ReturnType<typeof setTimeout>을 사용해주었습니다.

--- src/components/TimePicker/index.tsx
+++ src/components/TimePicker/index.tsx
@@ -1,6 +1,5 @@
 // 라이브러리
 import React, { useRef, useEffect, useState, useCallback } from 'react';
-import { throttle } from 'lodash';
 
 // 파일
 import * as _ from './style';
@@ -11,39 +10,36 @@ interface TimePickerProps {
 }
 
 const TimePicker = ({ list, onSelectedChange }: TimePickerProps) => {
-  const SCROLL_DEBOUNCE_TIME = 200;
+  const SCROLL_DEBOUNCE_TIME = 100;
 
   const newList = ['', ...list, ''];
   const ref = useRef<HTMLUListElement>(null);
   const [selected, setSelected] = useState(1);
   const itemRefs = useRef<(HTMLLIElement | null)[]>([]);
-  const timerRef = useRef<NodeJS.Timeout | null>(null);
+  const timerRef = useRef<ReturnType<typeof setTimeout> | null>(null);
   const ITEM_HEIGHT = 50;
 
-  const handleScroll = useCallback(
-    throttle(() => {
-      if (ref.current) {
-        clearTimeout(timerRef.current!);
-        if (ref.current.scrollTop < ITEM_HEIGHT) {
-          ref.current.scrollTop = ITEM_HEIGHT;
-        }
-        timerRef.current = setTimeout(() => {
-          const index = Math.floor(
-            (ref.current!.scrollTop + ITEM_HEIGHT / 2) / ITEM_HEIGHT
-          );
-          if (list[index] !== '') {
-            setSelected(index);
-            itemRefs.current[index]?.scrollIntoView({
-              behavior: 'smooth',
-              block: 'center'
-            });
-            onSelectedChange && onSelectedChange(newList[index]);
-          }
-        }, SCROLL_DEBOUNCE_TIME);
+  const handleScroll = useCallback(() => {
+    if (ref.current) {
+      clearTimeout(timerRef.current!);
+      if (ref.current.scrollTop < ITEM_HEIGHT) {
+        ref.current.scrollTop = ITEM_HEIGHT;
       }
-    }),
-    [onSelectedChange]
-  );
+      timerRef.current = setTimeout(() => {
+        const index = Math.floor(
+          (ref.current!.scrollTop + ITEM_HEIGHT / 2) / ITEM_HEIGHT
+        );
+        if (list[index] !== '') {
+          setSelected(index);
+          itemRefs.current[index]?.scrollIntoView({
+            behavior: 'smooth',
+            block: 'center'
+          });
+          onSelectedChange && onSelectedChange(newList[index]);
+        }
+      }, SCROLL_DEBOUNCE_TIME);
+    }
+  }, [onSelectedChange, newList]);
 
   useEffect(() => {
     if (ref.current) {
