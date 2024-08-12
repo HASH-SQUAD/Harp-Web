fd5b7efbdd5924662d32fd18a6321e9e679341d7
추성우 <cntjddn715@gmail.com>
2024-08-13 01:25:02 +0900

    refactor(#78) :: searchPlace 메모제이션

--- src/components/Maps/MiniMap/index.tsx
+++ src/components/Maps/MiniMap/index.tsx
@@ -1,5 +1,5 @@
 // 라이브러리
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 
 // 파일
 import * as _ from './style';
@@ -20,7 +20,7 @@ interface Place {
 }
 
 const MiniMap = ({ keyword }: MiniMapProps) => {
-  useEffect(() => {
+  const searchPlace = useCallback(() => {
     const container = document.getElementById('map');
     const options = {
       center: new window.kakao.maps.LatLng(0, 0),
@@ -29,17 +29,22 @@ const MiniMap = ({ keyword }: MiniMapProps) => {
     const map = new window.kakao.maps.Map(container, options);
 
     const ps = new window.kakao.maps.services.Places();
-
-    const searchPlace = (data: Place[], status: string) => {
+    ps.keywordSearch(keyword, (data: Place[], status: string) => {
       if (status === window.kakao.maps.services.Status.OK && data.length > 0) {
         const place = data[0];
         const location = new window.kakao.maps.LatLng(place.y, place.x);
         map.setCenter(location);
       }
-    };
-
-    ps.keywordSearch(keyword, searchPlace);
+    });
   }, [keyword]);
+
+  useEffect(() => {
+    if (window.kakao) {
+      searchPlace();
+    } else {
+      console.log('카카오맵 스크립트 에러');
+    }
+  }, []);
 
   return <_.MiniMap_Layout id="map" />;
 };
