1d0aabb03fcbbda6caca436f9a13f10ce92e451c
rkdalswl718 <rkdalswl070718@gmail.com>
2024-08-17 02:47:02 +0900

    refactor :: 타입 변경

--- src/types/cropImage.ts
+++ src/types/cropImage.ts
@@ -1,10 +1,11 @@
 //src/types/cropImage.ts
 import { Area } from 'react-easy-crop/types';
 
+
 export interface CropImageProps {
   imageSrc: string;
   cropShape: 'rect' | 'round';
   aspectRatio: number;
   cropSize: { width: number; height: number };
-  onCropComplete?: (croppedAreaPixels: Area | null) => void;
+  onCropComplete?: (croppedImage: string) => void;
 }
