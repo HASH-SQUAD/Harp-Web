9a321bbe87112e9109e96e5b18592ab496ce2dc3
rkdalswl718 <rkdalswl070718@gmail.com>
2024-08-17 21:36:31 +0900

    chore :: 병합 오류 수정

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,23 +1,28 @@
 import { useRoutes } from 'react-router-dom';
 
-import Splash from '../pages/splash/';
+import Splash from 'pages/splash';
 import Auth from 'pages/auth';
 import Home from 'pages/home';
 import NotFound from 'pages/NotFound';
-import Crop from 'pages/crop'
+
 // register
 import Terms from 'pages/register/terms';
 import UserInfo from 'pages/register/userinfo';
 import SurveyStyle from 'pages/register/surveyStyle';
 import SurveyFood from 'pages/register/surveyFood';
 import SurveyMBTI from 'pages/register/surveyMBTI';
-import SurveryTMI from 'pages/register/surveyTMI';
+import SurveyTMI from 'pages/register/surveyTMI';
 import SurveyEnd from 'pages/register/surveyEnd';
+
 // plan
 import Chat from 'pages/plan/chat';
 import SelectDate from 'pages/plan/selectDate';
 import Info from 'pages/plan/info';
 import InfoCrop from 'pages/plan/crop';
+import Detail from 'pages/plan/memo';
+import Update from 'pages/plan/update';
+import AddSearch from 'pages/plan/addSearch'
+
 // profile
 import Edit from 'pages/profile/edit';
 import EditCrop from 'pages/profile/crop';
@@ -26,39 +31,40 @@ export default function Router() {
   return useRoutes([
     {
       path: '/',
+      element: <Home />,
       children: [
-        { path: '/', element: <Home /> },
         { path: 'splash', element: <Splash /> },
         { path: 'auth', element: <Auth /> },
-        { path: '*', element: <NotFound /> },
-        { path: 'crop', element: <Crop /> }
+        { path: '*', element: <NotFound /> }
       ]
     },
     {
-      path: '/register',
+      path: 'register',
       children: [
         { path: 'terms', element: <Terms /> },
         { path: 'userinfo', element: <UserInfo /> },
         { path: 'surveystyle', element: <SurveyStyle /> },
         { path: 'surveyfood', element: <SurveyFood /> },
         { path: 'surveymbti', element: <SurveyMBTI /> },
-        { path: 'surveytmi', element: <SurveryTMI /> },
+        { path: 'surveytmi', element: <SurveyTMI /> },
         { path: 'surveyend', element: <SurveyEnd /> }
       ]
     },
     {
-      path: '/plan',
+      path: 'plan',
       children: [
         { path: 'chat', element: <Chat /> },
         { path: 'selectdate', element: <SelectDate /> },
         { path: 'info/:id', element: <Info /> },
-        { path: 'info/:id/crop', element: <InfoCrop /> }
+        { path: 'info/:id/crop', element: <InfoCrop /> },
+        { path: 'info/:id/day/:planId', element: <Detail /> },
+        { path: 'info/:id/day/:planId/update', element: <Update /> },
+        { path: 'info/:id/day/:planId/addsearch', element: <AddSearch /> }
       ]
     },
     {
-      path: '/profile',
-      children: [
-        { path: 'edit', element: <Edit /> },
+      path: 'profile',
+      children: [{ path: 'edit', element: <Edit /> },
         { path: 'edit/crop', element: <EditCrop /> }
       ]
     }
