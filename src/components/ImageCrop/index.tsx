3dadde1b081268c66d292ba5a21226152e5f80ca
rkdalswl718 <rkdalswl070718@gmail.com>
2024-08-17 21:39:08 +0900

    chore :: 병합 오류 수정

--- src/components/ImageCrop/index.tsx
+++ src/components/ImageCrop/index.tsx
@@ -1,29 +1,46 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState } from 'react';
 import Cropper from 'react-easy-crop';
+import { Area } from 'react-easy-crop/types';
+import { getCroppedImg } from '../../lib/utils/cropImage';
+import NextButton from 'components/NextButton';
+import { CropImageProps } from '../../types/cropImage';
 
-function ImageCrop({ imageSrc, cropShape, onCropComplete }) {
+export const CropImage = ({ imageSrc, cropShape, aspectRatio, onCropComplete }: CropImageProps) => {
   const [crop, setCrop] = useState({ x: 0, y: 0 });
   const [zoom, setZoom] = useState(1);
+  const [croppedAreaPixels, setCroppedAreaPixels] = useState<Area | null>(null);
 
-  const handleCropComplete = useCallback((croppedArea, croppedAreaPixels) => {
-    // Crop 완료 후 처리 로직
-    onCropComplete(croppedAreaPixels);
-  }, [onCropComplete]);
+  const onCropCompleteHandler = (croppedArea: Area, croppedAreaPixels: Area) => {
+    setCroppedAreaPixels(croppedAreaPixels);
+  };
+
+  const handleComplete = async () => {
+    if (croppedAreaPixels) {
+      try {
+        const croppedImage = await getCroppedImg(imageSrc, croppedAreaPixels);
+        if (onCropComplete) {
+          onCropComplete(croppedImage);
+        }
+      } catch (error) {
+        console.error('이미지 자르기 오류:', error);
+      }
+    }
+  };
 
   return (
-    <div style={{ position: 'relative', width: '100%', height: '400px' }}>
-      <Cropper
-        image={imageSrc}
-        crop={crop}
-        zoom={zoom}
-        aspect={1} // 1:1 비율
-        cropShape={cropShape}
-        onCropChange={setCrop}
-        onZoomChange={setZoom}
-        onCropComplete={handleCropComplete}
-      />
+    <div style={{ position: 'relative', width: '100%', height: 'calc(100vh - 120px)', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      
+        <Cropper
+          image={imageSrc}
+          crop={crop}
+          zoom={zoom}
+          aspect={aspectRatio}
+          cropShape={cropShape}
+          onCropChange={setCrop}
+          onZoomChange={setZoom}
+          onCropComplete={onCropCompleteHandler}
+        />
+      <NextButton text="완료" state={!!croppedAreaPixels} onClick={handleComplete} />
     </div>
   );
-}
-
-export default ImageCrop;
+};
--- src/pages/crop/style.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const Crop_Container = styled.div `
-  padding: 0 20px;  display: flex;
-  width: 100%;
-  height: 100%;
-  flex-direction: column;
-`
