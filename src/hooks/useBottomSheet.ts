6f2bc1d941265d5ca1cdfb2b7a16f236fa5691a2
추성우 <cntjddn715@gmail.com>
2024-08-20 14:50:17 +0900

    chore(#21) :: useBottomSheet 구현

---
+++ src/hooks/useBottomSheet.ts
@@ -0,0 +1,51 @@
+import { useRef, useEffect } from 'react';
+
+interface BottomSheetMetrics {
+  touchStart: {
+    sheetY: number; // touchstart에서 BottomSheet의 최상단 모서리의 Y값
+    touchY: number; // touchstart에서 터치 포인트의 Y값
+  };
+  touchMove: {
+    prevTouchY?: number; // 다음 touchmove 이벤트 핸들러에서 필요한 터치 포인트 Y값을 저장
+    movingDirection: 'none' | 'down' | 'up'; // 유저가 터치를 움직이고 있는 방향
+  };
+}
+
+export function useBottomSheet() {
+  const sheet = useRef<HTMLDivElement>(null);
+
+  const metrics = useRef<BottomSheetMetrics>({
+    touchStart: {
+      sheetY: 0,
+      touchY: 0
+    },
+    touchMove: {
+      prevTouchY: 0,
+      movingDirection: 'none'
+    }
+  });
+
+  // Touch Event 핸들러들을 등록한다.
+  useEffect(() => {
+    const handleTouchStart = (e: TouchEvent) => {};
+
+    const handleTouchMove = (e: TouchEvent) => {};
+
+    const handleTouchEnd = (e: TouchEvent) => {};
+    if (sheet.current) {
+      sheet.current.addEventListener('touchstart', handleTouchStart);
+      sheet.current.addEventListener('touchmove', handleTouchMove);
+      sheet.current.addEventListener('touchend', handleTouchEnd);
+    }
+
+    return () => {
+      if (sheet.current) {
+        sheet.current.removeEventListener('touchstart', handleTouchStart);
+        sheet.current.removeEventListener('touchmove', handleTouchMove);
+        sheet.current.removeEventListener('touchend', handleTouchEnd);
+      }
+    };
+  }, []);
+
+  return { sheet };
+}
