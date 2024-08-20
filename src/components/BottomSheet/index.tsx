843cd03b3cc34a6d875ec6668e56b88dfc3c79ec
추성우 <cntjddn715@gmail.com>
2024-08-20 14:43:59 +0900

    chore(#21) :: 모달 구조

---
+++ src/components/BottomSheet/index.tsx
@@ -0,0 +1,17 @@
+// 라이브러리
+import React from 'react';
+
+// 파일
+import * as _ from './style';
+
+const BottomSheet = () => {
+  return (
+    <_.BottomSheet_Layout>
+      <_.BottomSheet_Wrapper>
+        <_.BottomSheet_Handle />
+      </_.BottomSheet_Wrapper>
+    </_.BottomSheet_Layout>
+  );
+};
+
+export default BottomSheet;
--- src/pages/plan/map/index.tsx
+++ src/pages/plan/map/index.tsx
@@ -5,6 +5,7 @@ import React, { useEffect } from 'react';
 import * as _ from './style';
 import Header from 'components/Header';
 import ScreenMap from 'components/Maps/ScreenMap';
+import BottomSheet from 'components/BottomSheet';
 
 const Map = () => {
   return (
@@ -12,6 +13,7 @@ const Map = () => {
       <Header title="지도" buttonState="완료" />
       <_.Map_Layout>
         <ScreenMap />
+        <BottomSheet />
       </_.Map_Layout>
     </>
   );
