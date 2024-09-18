7bc4f064b24bcfa7fa4e3468a6a8058e16720e77
추성우 <cntjddn715@gmail.com>
2024-09-18 23:41:34 +0900

    chore(#96) :: surveyMbti 전역 관리

--- src/atoms/user.ts
+++ src/atoms/user.ts
@@ -32,3 +32,20 @@ export const checkedFoodsState = atom({
     foods: SurveyFoodData.map((item) => ({ id: item.id, state: false }))
   }
 });
+
+export const selectedMBTIState = atom({
+  key: 'selectedMBTIState',
+  default: {
+    mbti: [
+      { id: 1, left: 'E', right: 'I', text: '내향형 / 외향형', state: false },
+      { id: 2, left: 'S', right: 'N', text: '직관형 / 감각형', state: false },
+      { id: 3, left: 'T', right: 'F', text: '사고형 / 감정형', state: false },
+      { id: 4, left: 'P', right: 'J', text: '인식형 / 판단형', state: false }
+    ]
+  }
+});
+
+export const stringMbtiState = atom({
+  key: 'stringMbtiState',
+  default: ''
+});
