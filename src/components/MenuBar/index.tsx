20b40ad612ff006a037bd5dc580c78e05076e35a
추성우 <cntjddn715@gmail.com>
2024-09-19 17:23:34 +0900

    add :: 메뉴바 이동

--- src/components/MenuBar/index.tsx
+++ src/components/MenuBar/index.tsx
@@ -1,85 +1,59 @@
-//라이브러리
+// 라이브러리
 import React from 'react';
 
-//파일
+// 파일
 import * as _ from './style';
 import Home from 'assets/Icon/MenuBar/Select/Home';
 import Home_Not from 'assets/Icon/MenuBar/NotSelect/Home';
 import Travel from 'assets/Icon/MenuBar/Select/Travel';
 import Travel_Not from 'assets/Icon/MenuBar/NotSelect/Travel';
 import Community from 'assets/Icon/MenuBar/Select/Community';
-import Community_Not from 'assets/Icon/MenuBar/NotSelect/Travel';
+import Community_Not from 'assets/Icon/MenuBar/NotSelect/Community';
 import Wish from 'assets/Icon/MenuBar/Select/Wish';
 import Wish_Not from 'assets/Icon/MenuBar/NotSelect/Wish';
 import All from 'assets/Icon/MenuBar/Select/All';
 import All_Not from 'assets/Icon/MenuBar/NotSelect/All';
+import { useNavigate } from 'react-router-dom';
 
 interface MenuBarProps {
   selectState: number;
 }
 
+const icons = [
+  { selected: Home, notSelected: Home_Not, title: '홈', location: '/' },
+  {
+    selected: Travel,
+    notSelected: Travel_Not,
+    title: '여행',
+    location: '/plan/selectdate'
+  },
+  {
+    selected: Community,
+    notSelected: Community_Not,
+    title: '커뮤니티',
+    location: '/community'
+  },
+  { selected: Wish, notSelected: Wish_Not, title: '찜', location: '/wish' },
+  { selected: All, notSelected: All_Not, title: '전체', location: '/all' }
+];
+
 const MenuBar = ({ selectState }: MenuBarProps) => {
+  const navigate = useNavigate();
   return (
     <_.MenuBar_Container>
-      {selectState === 1 ? (
-        <_.Menubar_Icon>
-          <Home />
-          <_.Menubar_Title select>홈</_.Menubar_Title>
+      {icons.map((icon, index) => (
+        <_.Menubar_Icon
+          key={index}
+          onClick={() => {
+            navigate(icon.location);
+          }}
+        >
+          {selectState === index + 1 ? <icon.selected /> : <icon.notSelected />}
+          <_.Menubar_Title select={selectState === index + 1}>
+            {icon.title}
+          </_.Menubar_Title>
         </_.Menubar_Icon>
-      ) : (
-        <_.Menubar_Icon>
-          <Home_Not />
-          <_.Menubar_Title>홈</_.Menubar_Title>
-        </_.Menubar_Icon>
-      )}
-
-      {selectState === 2 ? (
-        <_.Menubar_Icon>
-          <Travel />
-          <_.Menubar_Title select>여행</_.Menubar_Title>
-        </_.Menubar_Icon>
-      ) : (
-        <_.Menubar_Icon>
-          <Travel_Not />
-          <_.Menubar_Title>여행</_.Menubar_Title>
-        </_.Menubar_Icon>
-      )}
-
-      {selectState === 3 ? (
-        <_.Menubar_Icon>
-          <Community />
-          <_.Menubar_Title select>커뮤니티</_.Menubar_Title>
-        </_.Menubar_Icon>
-      ) : (
-        <_.Menubar_Icon>
-          <Community_Not />
-          <_.Menubar_Title>커뮤니티</_.Menubar_Title>
-        </_.Menubar_Icon>
-      )}
-
-      {selectState === 4 ? (
-        <_.Menubar_Icon>
-          <Wish />
-          <_.Menubar_Title select>찜</_.Menubar_Title>
-        </_.Menubar_Icon>
-      ) : (
-        <_.Menubar_Icon>
-          <Wish_Not />
-          <_.Menubar_Title>찜</_.Menubar_Title>
-        </_.Menubar_Icon>
-      )}
-
-      {selectState === 5 ? (
-        <_.Menubar_Icon>
-          <All />
-          <_.Menubar_Title select>전체</_.Menubar_Title>
-        </_.Menubar_Icon>
-      ) : (
-        <_.Menubar_Icon>
-          <All_Not />
-          <_.Menubar_Title>전체</_.Menubar_Title>
-        </_.Menubar_Icon>
-      )}
+      ))}
     </_.MenuBar_Container>
   );
 };
