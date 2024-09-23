453fc28cf555116f266ba07b5ee9302f79efcb96
추성우 <cntjddn715@gmail.com>
2024-09-23 11:27:56 +0900

    chore(#98) :: planInfo로 이동

--- src/components/PlanResult/index.tsx
+++ src/components/PlanResult/index.tsx
@@ -6,17 +6,25 @@ import * as _ from './style';
 import Share from 'assets/Icon/Share';
 import { theme } from 'lib/utils/style/theme';
 import { formatSelectedDate } from 'lib/utils/formatSelectedDate';
+import { useNavigate } from 'react-router-dom';
 
 interface OwnProps {
+  id: string | undefined;
   img: string;
   title: string;
   startDate: string;
   member: string;
 }
 
-const PlanResult = ({ img, title, startDate, member }: OwnProps) => {
+const PlanResult = ({ id, img, title, startDate, member }: OwnProps) => {
+  const navigate = useNavigate();
+
   return (
-    <_.PlanResult_Layout>
+    <_.PlanResult_Layout
+      onClick={() => {
+        navigate(`/plan/info/${id}`);
+      }}
+    >
       <_.PlanResult_Container imgUrl={img}>
         <_.PlanResult_Title>{title}</_.PlanResult_Title>
         <_.PlanResult_Info>
